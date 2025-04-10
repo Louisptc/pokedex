8df7901e64cbd66a2933f40054ee3ec2855161de
Louisptc <louispatachon@gmail.com>
2025-04-10 15:07:25 +0200

    trop dur à expliquer mais la page détail marche (germain à écrit le code mais mal)

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,11 +1,32 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import { SearchBar } from './searchbar';
+import { use } from 'react';
+import { useState, useEffect } from 'react';
+import { SafeAreaView, Platform } from 'react-native';
 
-export function DetailsScreen({navigation}) {
+
+export default function DetailsScreen({ navigation, route }) {
+  const [pokemon, setPokemon] = useState(null);
+  useEffect(() => {
+    const { pokedexId } = route.params;
+    fetch(`https://tyradex.vercel.app/api/v1/pokemon/${pokedexId}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Réponse réseau non OK");
+        return res.json();
+      })
+      .then((data) => setPokemon(data))
+      .catch((err) => console.error("Erreur de fetch :", err));
+  }, [route.params]);
+
+  if (!pokemon) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      hello
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>{pokemon.name.fr}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
